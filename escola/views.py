89f1bcf95b257252c6401a349094f695abb24fc3
mtheusbrito <matheusbritodasilva@gmail.com>
2023-02-04 19:30:03 -0300

    Modelo de aluno e curso

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,9 @@
 from django.http import JsonResponse
-from api.client import Institution
+from emec.client import Institution
 def alunos(request) :
     if request.method == 'GET':
       # ies = Institution(2571)
       # ies.parse()
       # data = ies.get_full_data()
       aluno = {'id': 1, 'nome': 'Matheus'}
-      return JsonResponse(data)
+      return JsonResponse(aluno)
