401e67159f093a6891756cecf7632e3f2bcf8d4f
mtheusbrito <matheusbritodasilva@gmail.com>
2023-02-04 23:05:45 -0300

    Viewset

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,26 @@
 from django.http import JsonResponse
 from emec.client import Institution
-def alunos(request) :
-    if request.method == 'GET':
-      # ies = Institution(2571)
-      # ies.parse()
-      # data = ies.get_full_data()
-      aluno = {'id': 1, 'nome': 'Matheus'}
-      return JsonResponse(aluno)
+from escola.models import Aluno, Curso
+from serializer import AlunosSerializer, CursoSerializer
+from rest_framework import viewsets
+
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunosSerializer
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+
+# def alunos(request) :
+#     if request.method == 'GET':
+#       # ies = Institution(2571)
+#       # ies.parse()
+#       # data = ies.get_full_data()
+#       aluno = {'id': 1, 'nome': 'Matheus'}
+#       return JsonResponse(aluno)
